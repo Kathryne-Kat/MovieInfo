fc61bb00e425b98453bcca9e0d7a4c7bbaa9e1bc
Kathryne-Kov <112563496+Kathryne-Kat@users.noreply.github.com>
2023-02-23 11:42:30 +0200

    add edits

--- src/components/CastList/CastListItem.jsx
+++ src/components/CastList/CastListItem.jsx
@@ -2,16 +2,22 @@ import css from './cast.module.css';
 import PropTypes from 'prop-types';
 
 export const CastListItem = ({ character, name, profile_path }) => {
-  let photo = '';
-  if (profile_path === null || profile_path === undefined) {
-    photo = '/src/img/http-404-not-found-error.jpg';
-    //photo = '#';
-  } else {
-    photo = `https://image.tmdb.org/t/p/w200${profile_path}`;
-  }
   return (
     <li className={css.linkActors}>
-      <img className={css.actors} src={photo} alt={name} />
+      {!profile_path ? (
+        ''
+      ) : (
+        // <img
+        //   className={css.actors}
+        //   src={'/src/img/http-404-not-found-error.jpg'}
+        //   alt={name}
+        // />
+        <img
+          className={css.actors}
+          src={`https://image.tmdb.org/t/p/w200${profile_path}`}
+          alt={name}
+        />
+      )}
       <h4 className={css.name}>{name}</h4>
       <p>Character: {character}</p>
     </li>
--- src/components/MoviesList/MovieCard.jsx
+++ src/components/MoviesList/MovieCard.jsx
@@ -7,12 +7,20 @@ export const MovieCard = ({ id, title, poster_path }) => {
   const location = useLocation();
   //console.log(location);
 
-  if (!poster_path) return;
+  // if (!poster_path) return;
   const poster = `https://image.tmdb.org/t/p/w200${poster_path}`;
   return (
     <li className={css.poster}>
       <Link to={'/movies/' + id} state={{ from: location }}>
-        <img src={poster} alt="title" />
+        {!poster_path ? (
+          ''
+        ) : (
+          // <img
+          //   src={'/src/img/http-404-not-found-error.jpg'}
+          //   alt={title}
+          // />
+          <img src={poster} alt="title" />
+        )}
         <h2 className={css.title}>{title}</h2>
       </Link>
     </li>
@@ -25,11 +33,3 @@ MovieCard.propTypes = {
   poster_path: PropTypes.string.isRequired,
   vote_average: PropTypes.number.isRequired,
 };
-
-// let poster = '';
-// if (poster_path === null) {
-//   poster =
-//     './img/http-404-not-found-error-message-hypertext-vector-20025007.jpg';
-// } else {
-//   poster = `https://image.tmdb.org/t/p/w200${poster_path}`;
-// }
